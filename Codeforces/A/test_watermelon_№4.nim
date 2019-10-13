a029131576fac0c3733f30db249b85cdb5cabbb6
Dmitry Chekir <chekirdmitry@gmail.com>
2019-10-13 23:59:44 +0300

    № 4А Арбуз. Решение и тесты

---
+++ Codeforces/A/test_watermelon_№4.nim
@@ -0,0 +1,12 @@
+from watermelon_№4 import watermelon
+import unittest
+
+suite "watermelon":
+  test "YES":
+    for i in countUp(4, 100, 2):
+      check watermelon(i) == "YES"
+  
+  test "NO":
+    for i in countUp(1, 99, 2):
+      check watermelon(i) == "NO"
+    
