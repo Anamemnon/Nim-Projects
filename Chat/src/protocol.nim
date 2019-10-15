24cef04ad4af63b8700499fab35a840bf52dbe92
Dmitry Chekir <chekirdmitry@gmail.com>
2019-10-15 22:52:32 +0300

    Adds parseMessage procedure with unit tests

--- Chat/src/protocol.nim
+++ Chat/src/protocol.nim
@@ -1,7 +1,31 @@
+import json
+
 type
   Message* = object
     username*: string
     message*: string
 
-proc parseMessage* (dara: string): Message =
-  discard
+proc parseMessage* (data: string): Message =
+  let dataJSON = parseJson(data)
+  result.username = dataJSON["username"].getStr()
+  result.message = dataJSON["message"].getStr()
+
+when isMainModule:
+  block:
+    let data = """{"username": "John", "message": "Hi!"}"""
+    let parsed = parseMessage(data)
+    doAssert parsed.username == "John"
+    doAssert parsed.message == "Hi!"
+  
+  block:
+    let data = """foobar"""
+    try:
+      let parsed = parseMessage(data)
+      doAssert false
+    except JsonParsingError:
+      doAssert true
+    except:
+      doAssert false
+  
+  echo("All tests passed!")
+    
