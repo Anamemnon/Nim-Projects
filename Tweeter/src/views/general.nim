a23d5ebd72c4917d39934a9bb16f2bf2acccde9d
Dmitry Chekir <chekirdmitry@gmail.com>
2019-10-27 14:27:17 +0300

    renderTimeline

--- Tweeter/src/views/general.nim
+++ Tweeter/src/views/general.nim
@@ -1,5 +1,7 @@
 #? stdtmpl(subsChar = '$', metaChar = '#')
 #import xmltree
+#import ../database
+#import user
 #
 #proc `$!`(text: string): string = escape(text)
 #end proc
@@ -32,4 +34,20 @@
     <input type="submit" value="Login">
   </form>
 </div>
+#end proc
+#
+#proc renderTimeline*(username:string, messages: seq[Message]): string =
+#  result = ""
+<div id="user">
+  <h1>${$!username}'s timeline</h1>
+</div>
+<div id="newMessage">
+  <span>New message</span>
+  <form action="createMessage" method="post">
+    <input type="text" name="message">
+    <input type="hidden" name="username" value="${$!username}">
+    <input type="submit" name="Tweet">
+  </form>
+</div>
+${renderMessages(messages)}
 #end proc
