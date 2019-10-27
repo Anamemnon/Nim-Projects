6fff5e3117e19d04bed5881da420df62d5bc165c
Dmitry Chekir <chekirdmitry@gmail.com>
2019-10-27 17:16:29 +0300

    update root

--- Tweeter/src/tweeter.nim
+++ Tweeter/src/tweeter.nim
@@ -3,9 +3,19 @@ import jester
 import times
 import database, views/user, views/general
 
+let db = newDatabase()
+
 routes:
   get "/":
-    resp renderMain(renderLogin())
+    if request.cookies.hasKey("username"):
+      var user: User
+      if not db.findUser(request.cookies["username"], user):
+        user = User(username: request.cookies["username"], following: @[])
+        db.create(user)
+      let messages = db.findMessages(user.following)
+      resp renderMain(renderTimeline(user.username, messages))
+    else:
+      resp renderMain(renderLogin())
  
   post "/login":
     setCookie("username", @"username", getTime().getGMTime() + 2.hours)
