25e377e81347155a70fd5d5c4c7e95f2e420d5b6
Dmitry Chekir <chekirdmitry@gmail.com>
2019-10-27 22:21:38 +0300

    end

--- Tweeter/src/tweeter.nim
+++ Tweeter/src/tweeter.nim
@@ -5,20 +5,26 @@ import database, views/user, views/general
 
 let db = newDatabase()
 
+proc userLogin(db: Database, request: Request, user: var User): bool =
+  if request.cookies.hasKey("username"):
+    if not db.findUser(request.cookies["username"], user):
+      user = User(username: request.cookies["username"], following: @[])
+      db.create(user)
+    return true
+  else:
+    return false
+
 routes:
   get "/":
-    if request.cookies.hasKey("username"):
-      var user: User
-      if not db.findUser(request.cookies["username"], user):
-        user = User(username: request.cookies["username"], following: @[])
-        db.create(user)
-      let messages = db.findMessages(user.following)
+    var user: User
+    if db.userLogin(request, user):
+      let messages = db.findMessages(user.following & user.username)
       resp renderMain(renderTimeline(user.username, messages))
     else:
       resp renderMain(renderLogin())
  
   post "/login":
-    setCookie("username", @"username", getTime().getGMTime() + 2.hours)
+    setCookie("username", @"username", getTime().local() + 2.hours)
     redirect("/")
 
   post "/createMessage":
@@ -29,5 +35,28 @@ routes:
     )
     db.post(message)
     redirect("/")
+  
+  get "/@name":
+    cond '.' notin @"name"
+    var user: User
+    if not db.findUser(@"name", user):
+      halt "User not found"
+    
+    let messages = db.findMessages(@[user.username])
+    var currentUser: User
+    if db.userLogin(request, currentUser):
+      resp renderMain(renderUser(user, currentUser) & renderMessages(messages))
+    else:
+      resp renderMain(renderUser(user) & renderMessages(messages))
+
+  post "/follow":
+    var follower: User
+    var target: User
+    if not db.findUser(@"follower", follower):
+      halt "Follower not found"
+    if not db.findUser(@"target", target):
+      halt "Follow target not found"
+    db.follow(follower, target)
+    redirect(uri("/" & @"target"))
 
 runForever()
